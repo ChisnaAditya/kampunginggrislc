c8ef37f103ed85ada9f218cf880b48dc1b55f83f
AlgebraIbrahim <algebraibrahim@gmail.com>
2024-03-07 14:10:49 +0700

    add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,15 @@ export default function RootLayout({
           fbq('track', 'PageView');
           `}
         </Script>
+        <Script id="microsoft-clarity">
+          {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "lctya7ukcs")
+          `}
+        </Script>
       </Suspense>
       <body className={inter.className}>
         <div className="flex flex-col justify-between h-screen">
