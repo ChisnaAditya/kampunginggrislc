80627af795579ce7f867d98a92cc4b1f87218c41
AlgebraIbrahim <algebraibrahim@gmail.com>
2024-03-22 09:49:45 +0700

    add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,16 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-N3LXXW62')
         `}
         </Script>
+        <Script src="https://www.googletagmanager.com/ns.html?id=GTM-KG8RRC8G" />
+        <Script id="google-tag-manager">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KG8RRC8G')
+          `}
+        </Script>
         <Script id='tiktok-pixel'>
           {`
           !function (w, d, t) {
