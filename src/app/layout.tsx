8becd9acd91a5dd906ad26909ca783d199a11152
AlgebraIbrahim <algebraibrahim@gmail.com>
2024-03-22 10:15:48 +0700

    delete satu tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,16 +48,6 @@ export default function RootLayout({
           gtag('config', 'G-SC1KTK3NMP');
         `}
         </Script>
-        <Script src="https://www.googletagmanager.com/ns.html?id=GTM-N3LXXW62" />
-        <Script id="google-analytics">
-          {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N3LXXW62')
-        `}
-        </Script>
         <Script src="https://www.googletagmanager.com/ns.html?id=GTM-KG8RRC8G" />
         <Script id="google-tag-manager">
           {`
