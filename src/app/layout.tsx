eb7b258d49b8b1e9ea2761af9341f0bd90aac017
lcgoesinternational <lcgoesinternational@MacBook-Air-lcgoesinternational.local>
2024-07-29 13:59:48 +0700

    optimizing LCP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Suspense>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-HMS8J1N0MY" />
-        <Script id="google-analytics">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HMS8J1N0MY"
+        />
+        <Script async id="google-analytics">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -32,7 +35,7 @@ export default function RootLayout({
           gtag('config', 'G-HMS8J1N0MY');
         `}
         </Script>
-        <Script id="tiktok-pixel">
+        <Script async id="tiktok-pixel">
           {`
           !function (w, d, t) {
             w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
@@ -42,7 +45,7 @@ export default function RootLayout({
           }(window, document, 'ttq');
         `}
         </Script>
-        <Script id="meta-pixel">
+        <Script async id="meta-pixel">
           {`
           !function(f,b,e,v,n,t,s){
             if(f.fbq)return;
@@ -64,7 +67,7 @@ export default function RootLayout({
           fbq('track', 'PageView');
           `}
         </Script>
-        <Script id="microsoft-clarity">
+        <Script async id="microsoft-clarity">
           {`
           (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
