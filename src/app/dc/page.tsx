ba751dc1c8b178374f678b24db0bc1178708b048
lcgoesinternational <lcgoesinternational@MacBook-Air-lcgoesinternational.local>
2024-11-06 22:43:22 +0700

    add metadata dc

--- src/app/dc/page.tsx
+++ src/app/dc/page.tsx
@@ -1,13 +1,21 @@
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
-// import Hero from "./hero";
 import Banner from "./banner";
-// import Paket from "./paket";
 import Fasilitas from "./fasilitas";
 import Teacher from "./teachers";
 import FooterDC from "./footer";
 
 const Hero = dynamic(() => import("./hero"));
 const Paket = dynamic(() => import("./paket"), { ssr: false });
+
+export const metadata: Metadata = {
+  title: "Program Desember Ceria Kampung Inggris LC",
+  description:
+    "Nikmati momen liburan tak terlupakan di Kampung Inggris LC bersama teacher asik, teman baru dari seluruh Indonesia, bonus outbound ke SLG!",
+  verification: {
+    google: "twVlJo9pXVv3uqBNwTDAY9Zn6o-zvL3U-npaG5mLeAA",
+  },
+};
 
 export default function DesemberCeria() {
   return (
