bf0be399023b75f8774d8409dc3b879420e2e35f
lcgoesinternational <lcgoesinternational@MacBook-Air-lcgoesinternational.local>
2024-11-14 10:02:54 +0700

    edit footer plus tambah cta

--- src/app/dc/footer.tsx
+++ src/app/dc/footer.tsx
@@ -1,11 +1,21 @@
 "use client";
-import Image from "next/image";
 import { Button } from "@nextui-org/button";
 
 export default function FooterDC() {
   return (
-    <section className="bg-gradient-to-b from-[#fcce00b5] to-[#fca800d6]">
-      <div className="bg-footer-dc relative  py-10">
+    <section className="pt-20 bg-footer-dc">
+      <div className="bg-red-400/80 min-h-[300px] flex flex-col items-center justify-center lg:justify-evenly px-4">
+        <p className="max-w-lg text-center text-2xl lg:text-4xl text-slate-900 font-bold">
+          Jangan sia-siakan liburanmu cuma buat rebahan.
+        </p>
+        <Button
+          className="bg-secondaryLC text-black/70 lg:text-xl font-bold mt-5 lg:w-fit"
+          onClick={() => (window.location.href = "#paket")}
+        >
+          Ayo Daftar Sekarang!
+        </Button>
+      </div>
+      <div className=" relative  py-10">
         <p className="text-center text-xs px-10">
           Kampung Inggris LC @2024 All Right Reserved. Terms of use and Privacy
           Policy
