9b7baca75b61c8c5c5a35fd1bd4de356a780c644
lcgoesinternational <lcgoesinternational@MacBook-Air-lcgoesinternational.local>
2024-11-14 10:02:15 +0700

    edit cta

--- src/app/dc/cta.tsx
+++ src/app/dc/cta.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function CTA() {
   return (
     <div
-      className="lg:relative min-h-[150px] lg:min-h-[300px]"
+      className="lg:relative min-h-[150px] lg:min-h-[300px] py-20"
       onClick={() => (window.location.href = "#paket")}
     >
       <Image
@@ -12,7 +12,7 @@ export default function CTA() {
         src="/icon/dc/cta banner.jpg"
         width={500}
         height={500}
-        className="cursor-pointer w-full rounded-lg shadow-2xl transition duration-200 ease-out transform translate-x-0 translate-y-0 hover:-translate-x-1 hover:-translate-y-1"
+        className="cursor-pointer w-full"
       />
     </div>
   );
